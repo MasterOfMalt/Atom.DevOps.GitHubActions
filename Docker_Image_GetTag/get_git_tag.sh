4fa142195af58d2f59bcbfde32963147d5de4dde
monkers <mikem@msquaredconsulting.co.uk>
2021-03-19 15:43:59 +0000

    Pipeline tested

---
+++ Docker_Image_GetTag/get_git_tag.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+set -x
+env
+
+GITHUB_REF=${GITHUB_REF:-""}
+GITHUB_HEAD_REF=${GITHUB_HEAD_REF:-""}
+
+REF="$GITHUB_REF"
+if [ "$GITHUB_HEAD_REF" != "" ]; then
+  REF="$GITHUB_HEAD_REF"
+fi
+if [ "$REF" == 'refs/heads/master' ]
+then
+  echo "::set-output name=tag_name::latest"
+else
+  echo "::set-output name=tag_name::$(basename "$REF")"
+fi
