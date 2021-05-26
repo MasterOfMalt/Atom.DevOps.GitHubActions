9bdc747f0d5e1d26150e861d77e5a5fd1b5f8191
dastaple <danny@odmsolutions.co.uk>
2021-05-26 09:58:51 +0100

    PLOPS-499 Sigh - looks like you have to build, and then keep this dist folder for the action to work.

--- Docker_Build_From_Cache/src/main.js
+++ Docker_Build_From_Cache/src/main.js
@@ -36,45 +36,50 @@ async function build_tagged_image(dockerfile, target_name, image_name_tag, cache
 }
 
 async function run() {
-    // This will start all cache jobs.
-    // It will assume build targets depend on their potential cache,
-    // and on earlier build targets - so they come out in order.
+    try {
+        // This will start all cache jobs.
+        // It will assume build targets depend on their potential cache,
+        // and on earlier build targets - so they come out in order.
 
-    const dockerfile = core.getInput("dockerfile");
-    const image_prefix = core.getInput("image_prefix");
-    const image_targets = core.getInput("image_targets").split(',');
-    const tag_name = core.getInput("tag_name");
-    const registry = core.getInput("registry");
+        const dockerfile = core.getInput("dockerfile");
+        const image_prefix = core.getInput("image_prefix");
+        const image_targets = core.getInput("image_targets").split(',');
+        const tag_name = core.getInput("tag_name");
+        const registry = core.getInput("registry");
 
-    // Set up per image target settings and cache
-    // start cache processes (no dependancies)
-    let build_targets = image_targets.map(target => {
-        const image_target_name = registry + image_prefix + target;
-        return {
-            target: target,
-            image_target_name: image_target_name,
-            image_name_tag: image_target_name + ':' + tag_name,
-            cache_job: pull_image_cache(registry, image_target_name, tag_name )
-        }
-    });
+        // Set up per image target settings and cache
+        // start cache processes (no dependancies)
+        let build_targets = image_targets.map(target => {
+            const image_target_name = registry + image_prefix + target;
+            return {
+                target: target,
+                image_target_name: image_target_name,
+                image_name_tag: image_target_name + ':' + tag_name,
+                cache_job: pull_image_cache(registry, image_target_name, tag_name )
+            }
+        });
 
-    // start build processes (depend on the cache target and previous build)
-    let previous_build = null;
-    build_targets.each(async (build_target)=> {
-       if(previous_build) {
-           // Wait for preceding build
-           await previous_build;
-       }
-       // Wait for cache pull
-       let cache_setting = await build_target.cache_job;
-       // build and tag this target
-       previous_build = build_tagged_image(dockerfile, build_target.target, build_target.image_name_tag,
-           cache_setting );
-    });
-    await previous_build;
+        // start build processes (depend on the cache target and previous build)
+        let previous_build = null;
+        build_targets.each(async (build_target)=> {
+           if(previous_build) {
+               // Wait for preceding build
+               await previous_build;
+           }
+           // Wait for cache pull
+           let cache_setting = await build_target.cache_job;
+           // build and tag this target
+           previous_build = build_tagged_image(dockerfile, build_target.target, build_target.image_name_tag,
+               cache_setting );
+        });
+        await previous_build;
 
-    let output = build_targets.reduce( (accumulator, currentValue) => accumulator + "," + currentValue.image_name_tag, "");
-    core.setOutput("image_name_tags", output);
+        let output = build_targets.reduce( (accumulator, currentValue) => accumulator + "," + currentValue.image_name_tag, "");
+        core.setOutput("image_name_tags", output);
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run()
